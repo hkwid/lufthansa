2b53d1c13432cc4336f558e56754dd3c3c8b3563
NagataHajime <hajime.nagata@ncreate.net>
2015-09-05 13:22:11 +0200

    add images and implement js

--- scripts/oi_animation.js
+++ scripts/oi_animation.js
@@ -1,1 +1,26 @@
-''use strict"
+$(document).ready(function() {
+	console.log("document ready");
+
+	$("#scenes .scene").css("display:none");
+
+	$("#btnSceneNext").click(function(){
+		refreshScene();
+	});
+
+	console.log('kita1');
+	console.log('kita2');
+	/*
+*/
+});
+
+function refreshScene(){
+	console.log('kita');
+	var selecter = "#scenes .scene:eq(0)";
+//	var selecter = "#scenes .scene:eq("+currentSceneId+")";
+	$(selecter).css("display:block");
+}
+/*
+*/
+var currentSceneId = 0;
+
+
